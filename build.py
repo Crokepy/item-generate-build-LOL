0077aaed6d94520db7bd5856bbe85f86fb6351f5
Crokepy <70830384+Crokepy@users.noreply.github.com>
2023-04-23 03:57:40 -0400

    bug in select items fix (i think, keep on test)

--- build.py
+++ build.py
@@ -9,6 +9,8 @@ for i in range(5):
     filtered_items = [item for item in data["data"].values() if "name" in item and "gold" in item and item["gold"]["total"] > 2000]
     if filtered_items:
         selected_item = random.choice(filtered_items)
+        if selected_item == selected_item:
+            selected_item = random.choice(filtered_items)
         print(f"Item {i+1}: {selected_item['name']}, Total gold: {selected_item['gold']['total']}")
     else:
         print(f"Item {i+1}: No items found.")
